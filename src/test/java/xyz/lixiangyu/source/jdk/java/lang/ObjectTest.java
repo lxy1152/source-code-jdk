9698b0228bb2d15ff35bcef7850ca945728445a0
Xiangyu Li <2676092082@qq.com>
2021-06-25 21:07:55 +0800

    test: 测试代码提交
    测试代码提交

---
+++ src/test/java/xyz/lixiangyu/source/jdk/java/lang/ObjectTest.java
@@ -0,0 +1,14 @@
+package xyz.lixiangyu.source.jdk.java.lang;
+
+import org.junit.Test;
+
+/**
+ * @author lixiangyu
+ */
+public class ObjectTest {
+    @Test
+    public void test() {
+        Object obj = new Object();
+        System.out.println(obj.hashCode());
+    }
+}
